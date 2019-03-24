f800cd7c7efe0c4569f6546b888f010a3ea8059d
Evan Cooper <coopere5@users.noreply.github.com>
2019-03-24 15:47:35 -0400

    Release 2.0 (#9)
    * initial commit
    see https://github.com/benjamintruth/benjamintruth.github.io/commits/master
    
    * initial clean up/formatting pass
    
    * comment out footer for now
    
    * delete two files still in development in prep for prod push
    
    * reformat css
    
    * cut out references to georgeSoros.js (404)
    
    * comment out campaigns list for now
    
    * update events iframe
    
    * rollback 39ebfb8e6c4203b5b6dd63ce040fb4f5b97daa66
    
    * rename events to calendar
    
    * add a margin to ul for the list of campaigns
    
    * make font color consistent on links for the contact page
    
    * readd newsletter files to branch
    
    * readd favicon.png from master
    
    * use dsa logo as favicon
    
    * pull from fork to branch
    
    https://github.com/coopere5/coopere5.github.io

---
+++ scripts/DSApp.js
@@ -0,0 +1,54 @@
+var DSApp = angular.module('DSApp', [
+
+    "ngRoute"
+
+]);
+
+//routing setup
+DSApp.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "views/content/home.html"
+        })
+        .when("/home", {
+            templateUrl: "views/content/home.html"
+        })
+        .when("/about", {
+            templateUrl: "views/content/about.html"
+        })
+        .when("/calendar", {
+            templateUrl: "views/content/calendar.html"
+        })
+        .when("/contact", {
+            templateUrl: "views/content/contact.html"
+        })
+        .when("/join", {
+            templateUrl: "views/content/join.html"
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+
+});
+
+
+
+//directives for header and footer
+
+DSApp.directive("header", function () {
+    return {
+        restrict: 'EA',
+        templateUrl: "views/header.html"
+    };
+});
+
+
+DSApp.directive("footer", function () {
+    return {
+        restrict: 'EA',
+        templateUrl: "views/footer.html"
+    };
+});
+
+
+
